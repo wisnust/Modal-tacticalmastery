642ed0fb0fe94c20aa6b7625117d90e7c6425fcd
wisnust <wisnust10@gmail.com>
2016-08-20 14:02:54 +0700

    first commit

---
+++ js/modal-form.js
@@ -0,0 +1,135 @@
+
+$(document).ready(function() {
+
+  $('input[type=number]').on('keydown', function (e) {
+    e = (e) ? e : window.event;
+    var charCode = (e.which) ? e.which : e.keyCode;
+    var availableChar = [8, 18, 33, 34, 35, 36, 37, 38, 39, 40, 46];
+    if (availableChar.indexOf(charCode) != -1) {
+      return true;
+    }
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  });
+
+  $('input[type=tel]').on('keydown', function (e) {
+    e = (e) ? e : window.event;
+    var charCode = (e.which) ? e.which : e.keyCode;
+    if (charCode == 189)  {
+      return false;
+    }
+    return true;
+  });
+  $('#checkoutForm')
+      .on('init.field.fv', function (e, data) {
+        var field = data.field,        // Get the field name
+            $field = data.element,      // Get the field element
+            bv = data.fv;           // FormValidation instance
+
+        // Create a span element to show valid message
+        // and place it right before the field
+        var $span = $('<small/>')
+            .addClass('help-block validMessage text-success')
+            .attr('data-field', field)
+            .insertAfter($field)
+            .hide();
+
+        // Retrieve the valid message via getOptions()
+        var message = bv.getOptions(field).validMessage;
+        if (message) {
+          $span.html(message);
+        }
+      })
+      .formValidation({
+        framework: 'bootstrap4',
+        icon: {
+          valid: 'fa fa-check',
+          invalid: 'fa fa-remove',
+          validating: 'fa fa-refresh'
+        },
+        autoFocus: true,
+        fields: {
+          // Name field
+          name: {
+            validMessage: 'Nice to meet you!',
+            validators: {
+              notEmpty: {
+                message: 'The name is required'
+              },
+              stringLength: {
+                max: 100,
+                message: 'The name must be less than 100 characters long. '
+              },
+              // regexp: {
+              //     regexp: /^[a-zA-Z' \.]+$/,
+              //     message: 'The name can only consist of alphabetical'
+              // }
+            }
+          },
+          // Email field
+          email: {
+            validMessage: 'We will email you a confirmation',
+            validators: {
+              notEmpty: {
+                message: 'The email address is required'
+              },
+              stringLength: {
+                min: 1,
+                max: 100,
+                message: 'The name must be less than 100 characters long. '
+              },
+              regexp: {
+                regexp: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                message: 'The value is not a valid email address'
+              }
+            }
+          },
+          // phone number field
+          phonenumber: {
+            validMessage: 'We will only contact you about your order',
+            validators: {
+              notEmpty: {
+                message: 'Please supply a phone number so we can call if there are any problems using this address.'
+              },
+              stringLength: {
+                min: 10,
+                message: 'Not a valid 10-digit US phone number.'
+              }
+            }
+          }
+        }
+      })
+      .on('err.field.fv', function(e, data) {
+      })
+      .on('success.validator.fv', function(e, data) {
+      })
+      .on('err.form.fv', function (e, data) {
+      })
+      .on('success.form.fv', function (e, data) {
+      })
+      .on('success.field.fv', function (e, data) {
+        var field = data.field,        // Get the field name
+            $field = data.element;      // Get the field element
+
+        // Show the valid message element
+        $field.next('.validMessage[data-field="' + field + '"]').show();
+ 
+      })
+      .on('err.field.fv', function (e, data) {
+        var field = data.field,        // Get the field name
+            $field = data.element;      // Get the field element
+
+        // Show the valid message element
+        $field.next('.validMessage[data-field="' + field + '"]').hide();
+      });
+
+});
+
+jQuery(function ($) {
+  $('input[name=phonenumber]').mask('000-000-0000', {'translation': {0: {pattern: /[0-9*-]/}}}); 
+  $('#buyModal').on('shown.bs.modal', function(event) {
+      $('#name').focus()
+  });
+});
